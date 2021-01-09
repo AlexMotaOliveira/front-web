da2b77201e4caaa254249d6ef3a33d5a683bba83
Alex Mota Brito Cezar de Oliveira <alexmbcdo@gmail.com>
2021-01-09 14:46:05 -0300

    update Readme

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,5 @@
 import axios from "axios";
 import { OrderPayLoad } from "./Order/types";
-axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.put['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
-
 
 const API_URL = process.env.REACT_APP_API_URL
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
